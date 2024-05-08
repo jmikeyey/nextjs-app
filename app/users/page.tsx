15d80f76c62e92029e88e175f72171d839c9742f
Mayki <aw8hurtz@gmail.com>
2024-05-08 18:33:54 +0800

    final output, but has an error: TypeError: Cannot read properties of undefined (reading 0) at users/page.tsx

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,11 +5,7 @@ import { Users } from '../types/userTypes'
 
 const UsersPage = async () => {
 
-  const res = await fetch(
-    "https://jsonplaceholder.typicode.com/users",
-    {cache: 'no-store'}
-  );
-  const users: Users[] = await res.json();
+  const users: Users[] = await getData();
 
   return (
     <>
@@ -17,9 +13,21 @@ const UsersPage = async () => {
       <p> {new Date().toLocaleTimeString()} </p>
       <ul>
         {users.map(user => <li key={user.id}>{user.name}</li> )}
+
       </ul>
     </>
   )
+}
+
+async function getData() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users", {cache: 'no-store'});
+
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
 }
 
 export default UsersPage
